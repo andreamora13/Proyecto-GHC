126bfaa9dea0ee5d635e33164465dc2fee853abd
unknown <andreamora1356gmail.com>
2021-10-09 15:27:45 -0500

    vf6

--- app/Http/Controllers/AdmiController.php
+++ app/Http/Controllers/AdmiController.php
@@ -200,8 +200,8 @@ class AdmiController extends Controller
         $partida= new App\Partida;
         $partida->activa=1;
         $partida->max_usuarios=6;
-        $partida->created_at="2021-10-01 02:22:13";
-        $partida->updated_at="2021-10-01 02:22:13";
+        $partida->created_at="2021-10-02 01:45:18";
+        $partida->updated_at="2021-10-02 01:45:18";
         $partida->save();
 
         $id_partida=App\Partida::select('id_partida')->get()->last();
